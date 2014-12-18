692db75aa7e1740ba837a05b53822b278c7183f2
Chris Loew <cloew123@gmail.com>
2014-12-18 09:26:57 -0700

    Can now view the words in a word list

--- src/Server/static/js/directives.js
+++ src/Server/static/js/directives.js
@@ -1,3 +1,15 @@
 'use strict';
 
-angular.module('VocabTesterDirectives', []);
+angular.module('VocabTesterDirectives', ['ui.bootstrap'])
+    .directive('wordCount', function() {
+      return {
+          restrict: 'E',
+          replace: true,
+          scope: {
+              words: '=words'
+          },
+          link: function(scope, elements, attrs) {
+              scope.isOpen = false;
+          },
+          templateUrl: 'static/partials/directives/word_count.html'
+      }})
