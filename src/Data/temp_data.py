41c645cb235a0ccb14ab1643c012dc31577c9265
Chris Loew <cloew123@gmail.com>
2014-12-19 09:20:18 -0700

    Moved data into a separate file temporarily

---
+++ src/Data/temp_data.py
@@ -0,0 +1,15 @@
+from Data.concept_manager import ConceptManager
+from Data.word_list import WordList
+
+from Quiz.quiz import Quiz
+from Quiz.quizzer import Quizzer
+
+cm = ConceptManager(["resources/days_of_week_english.json", "resources/days_of_week_japanese.json"])
+wordList = WordList("Days of the Week", range(1,8), "English", "Japanese")
+quizzer = None
+
+def BuildQuiz():
+    """ Build the Quiz """
+    global quizzer, wordList, cm
+    quizzer = Quizzer(Quiz(wordList, cm))
+    return quizzer
--- src/Server/word_lists_controller.py
+++ src/Server/word_lists_controller.py
@@ -1,15 +1,12 @@
 from word_list_wrapper import WordListWrapper
 
-from Data.concept_manager import ConceptManager
-from Data.word_list import WordList
+from Data.temp_data import wordList, cm
 
 from kao_flask.controllers.json_controller import JSONController
 
 class WordListsController(JSONController):
     """ Controller to return the word lists """
-    cm = ConceptManager(["resources/days_of_week_english.json", "resources/days_of_week_japanese.json"])
     
     def performWithJSON(self):
         """ Convert the existing Word Lists to JSON """
-        wordList = WordList("Days of the Week", range(1,8), "English", "Japanese")
-        return {"words":[WordListWrapper(wordList).toJSON(self.cm)]}
+        return {"words":[WordListWrapper(wordList).toJSON(cm)]}
