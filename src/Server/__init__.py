c28a4dc20a0c07c2742de7faddb03ebcd5646f4a
Chris Loew <cloew123@gmail.com>
2014-12-29 11:14:13 -0700

    Added basic Flask configuration and Sql Alchemy integration

--- src/Server/__init__.py
+++ src/Server/__init__.py
@@ -1,5 +1,9 @@
 from routes import routes
+from kao_flask.sqlalchemy_server import SqlAlchemyServer
+import os
 
-from kao_flask.server import Server
+os.environ["DATABASE_URL"]="postgresql://localhost/wordcount_dev"
+os.environ["APP_SETTINGS"]="Server.config.DevelopmentConfig"
 
-server = Server(__name__, routes=routes)
+
+server = SqlAlchemyServer(__name__, os.environ['APP_SETTINGS'], routes=routes)
