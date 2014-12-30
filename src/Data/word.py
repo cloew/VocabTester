d483bacd96e49970a4a3ae5c2e9ed3c340b3b197
Chris Loew <cloew123@gmail.com>
2014-12-29 17:14:18 -0700

    Added foreign key relationships to the word

--- src/Data/word.py
+++ src/Data/word.py
@@ -1,15 +1,22 @@
 from kao_flask.database import db
 
+from concept import Concept
+from language import Language
+
 class Word(db.Model):
     """ Represents a word from a particular language """
     __tablename__ = 'words'
     
     id = db.Column(db.Integer, primary_key=True)
     text = db.Column(db.String())
+    concept_id = db.Column(db.Integer, db.ForeignKey('concepts.id'))
+    concept = db.relationship("Concept", backref=db.backref('words'))
+    language_id = db.Column(db.Integer, db.ForeignKey('languages.id'))
+    language = db.relationship("Language", backref=db.backref('languages'))
     
-    def __init__(self, conceptId, text):
-        """ Initialize the word with its concept id and the text """
-        self.conceptId = conceptId
+    def __init__(self, concept, text):
+        """ Initialize the word with its concept and the text """
+        self.concept = concept
         self.text = text
         
     def __unicode__(self):
