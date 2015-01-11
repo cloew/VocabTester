f9302340845f506aefb92ada14ce95c8a90a885a
Chris Loew <cloew123@gmail.com>
2015-01-10 20:28:32 -0700

    Updated word_answer so it tracks when the answer was created

--- src/Data/word_answer.py
+++ src/Data/word_answer.py
@@ -2,11 +2,18 @@ from kao_flask.database import db
 
 from word_mastery import WordMastery
 
+from datetime import datetime
+
 class WordAnswer(db.Model):
     """ Represents an answer for a word """
     __tablename__ = 'word_answers'
     
     id = db.Column(db.Integer, primary_key=True)
     correct = db.Column(db.Boolean)
+    createdDate = db.Column(db.DateTime, default=datetime.now)
     word_mastery_id = db.Column(db.Integer, db.ForeignKey('word_masteries.id'))
-    word_mastery = db.relationship("WordMastery", backref=db.backref('answers'))
+    word_mastery = db.relationship("WordMastery", backref=db.backref('answers', order_by=createdDate))
+    
+    def __repr__(self):
+        """ Return the String Representation """
+        return "<Answer: {0}:{1}>".format(self.correct, self.createdDate)
