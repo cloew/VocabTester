b7983f689c1fb734ebbef9e69f488c7fc7f775e0
Chris Loew <cloew123@gmail.com>
2015-01-12 23:31:22 -0700

    Tidied up the Server Config

--- src/Server/config.py
+++ src/Server/config.py
@@ -4,23 +4,20 @@ class Config(object):
     DEBUG = False
     TESTING = False
     CSRF_ENABLED = True
-    SECRET_KEY = 'this-really-needs-to-be-changed'
+    # Sql Alchemy Settings
+    SECRET_KEY = os.environ['SECRET_KEY']
     SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
-
 
 class ProductionConfig(Config):
     DEBUG = False
-
 
 class StagingConfig(Config):
     DEVELOPMENT = True
     DEBUG = True
 
-
 class DevelopmentConfig(Config):
     DEVELOPMENT = True
     DEBUG = True
-
 
 class TestingConfig(Config):
     TESTING = True
