520beb22d1448d3d21b157dbc5372e525a7a5e49
Chris Loew <cloew123@gmail.com>
2015-01-13 19:07:45 -0700

    Added base controller that requires authentication

--- src/Server/Controller/word_lists_controller.py
+++ src/Server/Controller/word_lists_controller.py
@@ -1,18 +1,13 @@
-from Server.decorators import requires_auth
 from Server.Data.word_list_wrapper import WordListWrapper
 
 from Data.word_list import WordList
 import Data.concept_manager as cm
 
-from kao_flask.controllers.json_controller import JSONController
+from auth_json_controller import AuthJSONController
 
-class WordListsController(JSONController):
+class WordListsController(AuthJSONController):
     """ Controller to return the word lists """
     
-    def __init__(self):
-        """ Initialize the JSON Controller with its required decorators """
-        JSONController.__init__(self, decorators=[requires_auth])
-        
     def performWithJSON(self):
         """ Convert the existing Word Lists to JSON """
         wordLists = WordList.query.all()
