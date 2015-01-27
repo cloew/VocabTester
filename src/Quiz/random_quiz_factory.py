bb0af02c5b0c0b69eb712f55bbb3c0e197154b1c
Chris Loew <cloew123@gmail.com>
2015-01-27 11:23:51 -0700

    Can now construct a random quiz

---
+++ src/Quiz/random_quiz_factory.py
@@ -0,0 +1,18 @@
+from quiz import Quiz
+import random
+
+class RandomQuizFactory:
+    """ Represents method to contstruct a Quiz from random learned Words or Symbols """
+    MAX_QUESTIONS = 10
+    
+    def buildQuiz(self, formModel, user):
+        """ Build a quiz using the from given and the user provided """
+        learnedForms = user.getLearnedFor(formModel)
+        sample = random.sample(learnedForms, self.numberOfQuestions(learnedFroms))
+        conceptIds = [form.concept_id for form in sample]
+        pairs = self.conceptManager.getConceptPairs(conceptIds, user)
+        return Quiz("Random List", pairs)
+        
+    def numberOfQuestions(self, learnedFroms):
+        """ Returns the number of questions allowed for this quiz """
+        return min(len(learnedForms), self.MAX_QUESTIONS)
