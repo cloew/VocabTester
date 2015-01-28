853b07c6d60b4d04cacca0440b9fbf9458ca0e0c
Chris Loew <cloew123@gmail.com>
2015-01-27 23:58:02 -0700

    Random Quiz now weights lower mastery forms so they appear more frequently

--- src/Quiz/random_quiz_factory.py
+++ src/Quiz/random_quiz_factory.py
@@ -5,16 +5,65 @@ from sampler import sample_at_most
 class RandomQuizFactory:
     """ Represents method to contstruct a Quiz from random learned Words or Symbols """
     MAX_QUESTIONS = 10
+    RATING_RATIOS = [(0, .4),
+                     (1, .3),
+                     (2, .2),
+                     (3, .1)]
     
     def buildQuiz(self, formModel, user):
         """ Build a quiz using the from given and the user provided """
         conceptManager = ConceptManager(formModel)
         
         learnedForms = user.getLearnedFor(formModel)
-        sample = sample_at_most(learnedForms, self.MAX_QUESTIONS)
+        formsByRating = self.organizeByMastery(learnedForms, user)
+        sample = self.getSampleForQuiz(formsByRating)
         conceptIds = [form.concept_id for form in sample]
         pairs = conceptManager.getConceptPairs(conceptIds, user)
         
         return Quiz("Random List", pairs)
+        
+    def organizeByMastery(self, learnedForms, user):
+        """ Return the learned forms organized by their mastery rating """
+        forms = {}
+        for form in learnedForms:
+            rating = form.getMasteryRating(user)
+            if rating not in forms:
+                forms[rating] = [form]
+            else:
+                forms[rating].append(form)
+        return forms
+        
+    def getSampleForQuiz(self, formsByRating):
+        """ Return a sample with forms weighted via their mastery rating """
+        numberPerRating = self.getNumberPerRating(formsByRating)
+        sample = []
+        
+        for rating in numberPerRating:
+            if rating in formsByRating:
+                entries = sample_at_most(formsByRating[rating], numberPerRating[rating])
+                sample += entries
+        return sample
+        
+    def getNumberPerRating(self, formsByRating):
+        """ Return the number of entries for each rating that should be used """
+        numberPerRating = {}
+        hasMoreForms = {}
+        for rating, weight in self.RATING_RATIOS:
+            hasMoreForms[rating] = rating in formsByRating
+            if hasMoreForms[rating]:
+                numberPerRating[rating] = min(len(formsByRating[rating]), int(weight*self.MAX_QUESTIONS))
+            else:
+                numberPerRating[rating] = 0
+            
+        while sum(numberPerRating.values()) < self.MAX_QUESTIONS and any(hasMoreForms.values()):
+            for rating, weight in self.RATING_RATIOS:
+                if hasMoreForms[rating]:
+                    amountLeftover = self.MAX_QUESTIONS - sum(numberPerRating.values())
+                    numberThatCanBeAdded = len(formsByRating[rating]) - numberPerRating[rating]
+                    
+                    numberPerRating[rating] += min(numberThatCanBeAdded, amountLeftover)
+                    hasMoreForms[rating] = not (numberPerRating[rating] == len(formsByRating[rating]))
+        
+        return numberPerRating
         
 RandomQuizFactory = RandomQuizFactory()
