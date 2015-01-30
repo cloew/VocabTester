a34e480b2d863147420977e9e6c4f609fa9a67f7
Chris Loew <cloew123@gmail.com>
2015-01-30 14:56:34 -0700

    Added ability to add staleness periods to the seed file

--- src/seed.py
+++ src/seed.py
@@ -2,10 +2,26 @@ from Server import server
 
 from import_eggs import ImportEggs
 
+from Data.staleness_period import StalenessPeriod
+
 import glob
 import os
 
 EGG_DIR = os.path.join(os.path.dirname(__file__), 'resources/eggs')
+
+def AddStalenessPeriods():
+    """ Add the staleness periods """
+    previousPeriod = None
+    for days in [180, 90, 30, 14, 7]:
+        if preivousPeriod is not None:
+            period = StalenessPeriod(days=days, next=previousPeriod)
+        else:
+            period = StalenessPeriod(days=days)
+        previousPeriod = period
+        server.db.session.add(period)
+    period = StalenessPeriod(days=3, next=previousPeriod, first=True)
+    server.db.session.add(period)
+    server.db.session.commit()
 
 def GetListName(filename):
     """ Return the list name for the given file """
@@ -19,6 +35,7 @@ def GetListName(filename):
 
 def main():
     with server.app.app_context():
+        AddStalenessPeriods()
         for filename in glob.glob(os.path.join(EGG_DIR, '*.json')):
             ImportEggs(filename, GetListName(filename))
             
