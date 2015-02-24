5a2e0dcc8238492e1e60a10323d949b062c9e116
Chris Loew <cloew123@gmail.com>
2015-02-23 19:50:03 -0700

    Added watch command

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,26 +2,33 @@ module.exports = function(grunt) {
 
   // Project configuration.
   grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json'),
-        project: {
-            src: 'src',
-            serverStatic: '<%= project.src %>/Server/static',
-            stylesheets: '<%= project.serverStatic %>/stylesheets',
-            js: '<%= project.serverStatic %>/js/*.js'
-        },
-        sass: {
-            dist: {
-                files: [{
-                    expand: true,
-                    cwd: '<%= project.stylesheets %>',
-                    src: ['*.scss'],
-                    dest: '<%= project.stylesheets %>',
-                    ext: '.css'
-                }]
-            }
+    pkg: grunt.file.readJSON('package.json'),
+    project: {
+        src: 'src',
+        serverStatic: '<%= project.src %>/Server/static',
+        stylesheets: '<%= project.serverStatic %>/stylesheets',
+        js: '<%= project.serverStatic %>/js/*.js'
+    },
+    sass: {
+        dist: {
+            files: [{
+                expand: true,
+                cwd: '<%= project.stylesheets %>',
+                src: ['*.scss'],
+                dest: '<%= project.stylesheets %>',
+                ext: '.css'
+            }]
         }
+    },
+    watch: {
+        sass: {
+            files: '<%= project.stylesheets %>/*.{scss,sass}',
+            tasks: ['sass']
+        }
+    }
   });
   grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-watch');
 
   // Default task(s).
   grunt.registerTask('default', ['sass']);
