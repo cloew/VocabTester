470743cf1847f31a173e3d119e8e7cada0389bef
Chris Loew <cloew123@gmail.com>
2015-04-25 13:34:53 -0600

    Added button to go from admin languages page to the new language page

--- src/Server/static/js/kao_rest.js
+++ src/Server/static/js/kao_rest.js
@@ -1,13 +1,17 @@
 (function(a) {
     'use strict';
     a.module('kao.rest', [])
-        .controller('ListController', function ($scope, $http, navService) {
+        .controller('ListController', function ($scope, $http, $location, navService) {
             $scope.records = [];
             $http.get(navService.getApiUrl()).success(function(data) {
                 $scope.records = data.records;
             }).error(function(error) {
                 console.log(error);
             });
+            
+            $scope.goTo = function(path) {
+                $location.path(path);
+            };
         })
         .controller('NewController', function ($scope, $http, navService) {
             $scope.record = {};
