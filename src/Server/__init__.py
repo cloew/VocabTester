8f458ac2d862873e549debf103e34b12dc907789
Chris Loew <cloew123@gmail.com>
2015-05-30 12:51:06 -0600

    Actually use environment variables instead of hard-coding the values

--- src/Server/__init__.py
+++ src/Server/__init__.py
@@ -5,8 +5,4 @@ from kao_flask.ext.sqlalchemy.sqlalchemy_extension import SqlAlchemyExtension
 
 import os
 
-os.environ["DATABASE_URL"]="postgresql://postgres:postgres@localhost/vocab_dev"
-os.environ["APP_SETTINGS"]="Server.config.DevelopmentConfig"
-
-
 server = Server(__name__, config=os.environ["APP_SETTINGS"], routes=routes, extensions=[SqlAlchemyExtension()])
