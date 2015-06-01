97c33294cf0aba9a1232221c71b10cc147b8fdf5
Chris Loew <cloew123@gmail.com>
2015-06-01 11:33:00 -0600

    Updated the clear_db script to properly empty the database without destroying the tables

--- src/clear_db.py
+++ src/clear_db.py
@@ -1,27 +1,16 @@
-from Data.concept import Concept
-from Data.language import Language
-from Data.word import Word
-from Data.symbol import Symbol
-from Data.concept_list import ConceptList, concept_list_concepts
-from Data.answer import Answer
-from Data.mastery import Mastery
-from Data.staleness_period import StalenessPeriod
-from Data.user import User, learned_symbols, learned_words
-
 from Server import server
 
+import contextlib
 import sys
 
 def main(args):
     """ Run the main file """
     with server.app.app_context():
-        for table in [concept_list_concepts, learned_symbols, learned_words]:
-            d = table.delete()
-            server.db.session.execute(d)
-    
-        for table in [ConceptList, Answer, Mastery, StalenessPeriod, Word, Symbol, Language, Concept, User]:
-            table.query.delete()
-        server.db.session.commit()
+        with contextlib.closing(server.db.engine.connect()) as con:
+            trans = con.begin()
+            for table in reversed(server.db.metadata.sorted_tables):
+                con.execute(table.delete())
+            trans.commit()
 
 if __name__ == "__main__":
     main(sys.argv[1:])
