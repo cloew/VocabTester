05737da11c4d6a874bd5c4ba48b9132f9a123130
Chris Loew <cloew123@gmail.com>
2015-06-03 09:46:16 -0600

    Now use the KaoFlask SQLAlchmey Extension Crud controllers and endpoints

--- src/Server/Controller/register_controller.py
+++ src/Server/Controller/register_controller.py
@@ -1,23 +1,19 @@
 from Data.user import User
 from Server.error import EMAIL_IN_USE
-from Server.Controller.create_controller import CreateController
 from Server.Controller.login_controller import LoginController
-from Server.helpers.record_value_provider import RecordValueProvider
 
 from Server.decorators import requires_auth
 
-from kao_flask.ext.sqlalchemy.database import db
+from kao_flask.ext.sqlalchemy import db, CreateController, RecordValueProvider
 
 from sqlalchemy.exc import IntegrityError
-
-
 
 class RegisterController(CreateController):
     """ Controller to register a user """
     
     def __init__(self):
         """ Initialize the Register Controller """
-        CreateController.__init__(self, User, recordValueProvider=RecordValueProvider({'nativeLanguage': lambda value: ('native_language_id', value['id'])}))
+        CreateController.__init__(self, User, None, recordValueProvider=RecordValueProvider({'nativeLanguage': lambda value: ('native_language_id', value['id'])}))
     
     def performWithJSON(self, json=None):
         """ Create a User record with the given credentials """
