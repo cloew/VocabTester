c30631b8be3d7250d67173a770a93f724112fd73
Chris Loew <cloew123@gmail.com>
2015-10-09 11:44:45 -0600

    Added Gruntfile to build sass and spider and moved sass files to a WebApp directory

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,36 +1,78 @@
 module.exports = function(grunt) {
 
-  // Project configuration.
-  grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
-    project: {
-        src: 'src',
-        serverStatic: '<%= project.src %>/Server/static',
-        stylesheets: '<%= project.serverStatic %>/stylesheets',
-        js: '<%= project.serverStatic %>/js/*.js'
-    },
-    sass: {
-        dist: {
-            files: [{
-                expand: true,
-                cwd: '<%= project.stylesheets %>',
-                src: ['*.scss'],
-                dest: '<%= project.stylesheets %>',
-                ext: '.css'
-            }]
-        }
-    },
-    watch: {
+    // Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        project: {
+            src: 'src',
+            webapp: '<%= project.src %>/WebApp',
+            spider: '<%= project.webapp %>/spider',
+            sass: '<%= project.webapp %>/stylesheets',
+            sassLib: '<%= project.webapp %>/stylesheets/lib',
+            serverStatic: '<%= project.src %>/Server/static',
+            stylesheets: '<%= project.serverStatic %>/stylesheets',
+            js: '<%= project.serverStatic %>/js'
+        },
+        bowercopy: {
+            options: {
+                srcPrefix: 'bower_components'
+            },
+            js: {
+                options: {
+                    destPrefix: 'src/Server/static/lib'
+                },
+                files: {
+                  'src/Server/static/lib': '*/dist/*.js'
+                }
+            },
+            css: {
+                files: [{
+                    expand: true,
+                    src: ['**/*.css'],
+                    dest: '<%= project.stylesheets %>',
+                    ext: '.css'
+                }]
+            },
+            scss: {
+                files: [{
+                    expand: true,
+                    src: ['**/*.scss'],
+                    dest: '<%= project.sassLib %>',
+                    ext: '.scss'
+                }]
+            }
+        },
         sass: {
-            files: '<%= project.stylesheets %>/*.{scss,sass}',
-            tasks: ['sass']
+            compile: {
+                files: [{
+                    expand: true,
+                    cwd: '<%= project.sass %>',
+                    src: ['**/*.scss'],
+                    dest: '<%= project.stylesheets %>',
+                    ext: '.css'
+                }]
+            }
+        },
+        spider_script: {
+            options: {
+                sourcemap: false
+            },
+            compile: {
+                files: [{
+                    expand: true,
+                    cwd: '<%= project.spider %>',
+                    src: ['**/*.spider'],
+                    dest: '<%= project.js %>',
+                    ext: '.js'
+                }]
+            }
         }
-    }
-  });
-  grunt.loadNpmTasks('grunt-contrib-sass');
-  grunt.loadNpmTasks('grunt-contrib-watch');
+    });
+    grunt.loadNpmTasks('grunt-bowercopy');
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-spider-script');
+    grunt.loadNpmTasks('grunt-contrib-watch');
 
-  // Default task(s).
-  grunt.registerTask('default', ['sass']);
-
+    // Default task(s).
+    grunt.registerTask('default', ['bowercopy', 'sass', 'spider_script']);
 };
