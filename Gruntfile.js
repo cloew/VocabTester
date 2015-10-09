34066418e7cb5cc50fde2cccb583d8a406e2be92
Chris Loew <cloew123@gmail.com>
2015-10-09 11:50:44 -0600

    Added App spider file

--- Gruntfile.js
+++ Gruntfile.js
@@ -75,4 +75,5 @@ module.exports = function(grunt) {
 
     // Default task(s).
     grunt.registerTask('default', ['bowercopy', 'sass', 'spider_script']);
+    grunt.registerTask('spider', ['spider_script']);
 };
