2c3c4355e0bd347a4f6afed37a3eb671edb998d0
Chris Loew <cloew123@gmail.com>
2015-10-21 09:36:11 -0600

    Added String Representation to Symbol

--- src/Data/symbol.py
+++ src/Data/symbol.py
@@ -21,3 +21,7 @@ class Symbol(db.Model):
     def __unicode__(self):
         """ Return the string representation of the Word """
         return unicode(self.text)
+        
+    def __repr__(self):
+        """ Return the String representation of the Symbol """
+        return "<Symbol({0}, {1})>".format(self.text, self.language.name)
