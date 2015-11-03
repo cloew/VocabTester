c6f0708f7c1c243cc58362a542a2b76086466cd7
Chris Loew <cloew123@gmail.com>
2015-11-03 14:33:15 -0700

    Tidied up the Form Endpoint

--- src/Server/helpers/form_endpoint.py
+++ src/Server/helpers/form_endpoint.py
@@ -1,9 +1,10 @@
 from .symbol_route_info import SymbolRouteInfo
 from .word_route_info import WordRouteInfo
 
-from kao_decorators import lazy_property
-from kao_flask import Endpoint
+from kao_decorators import lazy_property, proxy_for
+from kao_flask import Endpoint, Routes
 
+@proxy_for('_routes', ['register'])
 class FormEndpoint:
     """ Helper class to provide the Endpoints for both Symbol and Word Concept Forms """
     routeInfos = [SymbolRouteInfo, WordRouteInfo]
@@ -14,11 +15,11 @@ class FormEndpoint:
         self.methodToController = methodToController
         
     @lazy_property
-    def endpoints(self):
-        """ Return the underlying endpoints """
-        return [Endpoint(self.url.format(form=routeInfo.form, formList=routeInfo.formList), **{method:cls(routeInfo.info) for method, cls in self.methodToController.items()}) for routeInfo in self.routeInfos]
-        
-    def register(self, app):
-        """ Register the endpoints with the given app """
-        for endpoint in self.endpoints:
-            endpoint.register(app)
+    def _routes(self):
+        """ Return the underlying routes """
+        routes = Routes()
+        for routeInfo in self.routeInfos:
+            cleanedUrl = self.url.format(form=routeInfo.form, formList=routeInfo.formList)
+            viewFns = {method:cls(routeInfo.info) for method, cls in self.methodToController.items()}
+            routes.add(Endpoint(cleanedUrl, **viewFns))
+        return routes
