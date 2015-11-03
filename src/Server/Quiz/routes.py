b4129fc318c44d3829b3881060f31a851f3b42c1
Chris Loew <cloew123@gmail.com>
2015-11-03 14:39:59 -0700

    Moved the Random Quizzes to a more sensible endpoint

--- src/Server/Quiz/routes.py
+++ src/Server/Quiz/routes.py
@@ -4,5 +4,5 @@ from Server.helpers import FormEndpoint
 from kao_flask import Routes, Endpoint
 
 routes = Routes(FormEndpoint('/api/languages/<int:languageId>/{formList}/<int:listId>/quiz', get=GetQuiz),
-                FormEndpoint('/api/languages/<int:languageId>/{formList}/random/quiz', get=GetRandomQuiz),
+                FormEndpoint('/api/languages/<int:languageId>/{form}/random/quiz', get=GetRandomQuiz),
                 Endpoint('/api/mastery/<int:masteryId>/answer', post=AnswerQuestion()))
