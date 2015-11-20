18bd1c72746eaa210bc2dcdc65ea89bc922f2908
Chris Loew <cloew123@gmail.com>
2015-11-20 12:08:48 -0700

    Added the legacy Authentication system

--- src/Server/auth/routes.py
+++ src/Server/auth/routes.py
@@ -1,4 +1,5 @@
 from .auth import auth
+from .legacy_password_util import LegacyPasswordUtil
 from .Controllers import CreateUserEnrollment, GetUserEnrollments
 from ..helpers.admin_json_factory import toJson
 
@@ -6,8 +7,9 @@ from kao_flask import Routes, Endpoint
 from kao_flask.ext.sqlalchemy import RecordValueProvider
 
 nativeLanguageProvider = RecordValueProvider({'nativeLanguage': lambda value: ('native_language_id', value['id'])})
+util = LegacyPasswordUtil()
 
-routes = Routes(Endpoint('/api/login', post=auth.LoginController(toJson)),
+routes = Routes(Endpoint('/api/login', post=auth.LoginController(toJson, legacyUtils=[util])),
                 Endpoint('/api/users', post=auth.RegisterController(toJson, recordValueProvider=nativeLanguageProvider)),
                 Endpoint('/api/users/current', get=auth.CurrentUserController(toJson), put=auth.UpdateUserController(toJson, recordValueProvider=nativeLanguageProvider)),
                 Endpoint('/api/users/current/enrollments', get=GetUserEnrollments(), post=CreateUserEnrollment()))
