45ee44d6abf7078a88417d3f2ddb3591e2a0d80c
Chris Loew <cloew123@gmail.com>
2016-06-25 10:03:40 -0600

    Can now check if Concept Pairs are ambiguous

--- src/Data/Concept/concept_pair.py
+++ src/Data/Concept/concept_pair.py
@@ -11,6 +11,10 @@ class ConceptPair:
         """ Return the mastery for the foreign form for the user """
         return self.foreign.getMastery(user)
         
+    def ambiguousWith(self, other):
+        """ Return if this Symbol is ambiguous with the other Symbol """
+        return self.native.ambiguousWith(other.native) or self.foreign.ambiguousWith(other.foreign)
+        
     def __repr__(self):
         """ Return the String representation of the Concpet Pair """
         return "<ConceptPair({}, {})>".format(self.native, self.foreign)
