b015f6dd519e33ff9f453dd19484d56761b1fa52
Chris Loew <cloew123@gmail.com>
2016-07-06 17:11:01 -0600

    Added simple Ambiguity Loader that does nothing...

---
+++ src/Import/ambiguity_loader.py
@@ -0,0 +1,7 @@
+
+class AmbiguityLoader:
+    """ Loads Ambiguity Groups and assigns Symbols to them """
+    
+    def __init__(self, groups):
+        """ Initialize with the Groups to load """
+        self.groups = groups
