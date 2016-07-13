e497a2ecd24d4eb91e12803ea7b5805e2eeb65b6
Chris Loew <cloew123@gmail.com>
2016-07-12 18:03:07 -0600

    Adding initial Text Converter guy

---
+++ src/Language/Japanese/text_converter.py
@@ -0,0 +1,18 @@
+from .symbol_groups import HIRAGANA_MAP
+from cached_property import cached_property
+
+class TextConverter:
+    """ Helper class to manage converting Japanese Character strings into a form the User can understand based on their Learned Symbols """
+    
+    def __init__(self, symbols):
+        """ Initialize with the Symbols the User has learned """
+        self.symbols = {symbol.text for symbol in symbols}
+        
+    def convert(self, word):
+        """ Convert the given Word """
+        
+    @cached_property
+    def symbolMap(self):
+        """ Return the map of symbols that should be converted """
+        return {key:value for key, value in HIRAGANA_MAP.items() if key not in self.symbols}
+        
