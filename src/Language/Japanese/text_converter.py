b9c0924dbda270d139112537432c7b1cd4a01abd
Chris Loew <cloew123@gmail.com>
2016-07-21 18:32:07 -0600

    Now use the KaoRomKan library for text conversion

--- src/Language/Japanese/text_converter.py
+++ src/Language/Japanese/text_converter.py
@@ -1,21 +1,13 @@
-from .symbol_groups import HIRAGANA_MAP
-
-from cached_property import cached_property
-from kao_symbols import convert
+from kao_romkan import RomKanConverter
 
 class TextConverter:
     """ Helper class to manage converting Japanese Character strings into a form the User can understand based on their Learned Symbols """
     
     def __init__(self, symbols):
         """ Initialize with the Symbols the User has learned """
-        self.symbols = {symbol.text for symbol in symbols}
+        self.converter = RomKanConverter(symbol.text for symbol in symbols)
         
-    def convert(self, word):
+    def convert(self, word, readings={}):
         """ Convert the given Word """
-        return convert(word.text, self.symbolMap)
-        
-    @cached_property
-    def symbolMap(self):
-        """ Return the map of symbols that should be converted """
-        return {key:value for key, value in HIRAGANA_MAP.items() if key not in self.symbols}
+        return self.converter.convert(word.text, readings=readings)
         
