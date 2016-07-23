82a93e5c7a16bee2fcbb81e8f466f7e017716d98
Chris Loew <cloew123@gmail.com>
2016-07-23 09:04:26 -0600

    Added JSON column for the language specific data on a word

--- src/Data/Concept/word.py
+++ src/Data/Concept/word.py
@@ -3,6 +3,8 @@ from ..language import Language
 from ..Mastery import Mastery
 
 from kao_flask.ext.sqlalchemy import db
+
+from sqlalchemy.dialects.postgresql import JSON
 from sqlalchemy.ext.hybrid import hybrid_method
 
 class Word(db.Model):
@@ -11,9 +13,11 @@ class Word(db.Model):
     
     id = db.Column(db.Integer, primary_key=True)
     text = db.Column(db.UnicodeText())
+    language_data = db.Column(JSON)
     concept_id = db.Column(db.Integer, db.ForeignKey('concepts.id', ondelete="CASCADE"))
-    concept = db.relationship("Concept")
     language_id = db.Column(db.Integer, db.ForeignKey('languages.id', ondelete="CASCADE"))
+    
+    concept = db.relationship("Concept")
     language = db.relationship("Language")
     
     @hybrid_method
